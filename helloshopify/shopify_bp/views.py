38cc0f6aa2401a172c29febda9ad26e311617dac
Pushkar Paranjpe <pushkarparanjpe@gmail.com>
2019-10-13 09:06:26 +0530

    Added content r/w permissions to the Shopify app scope.

--- helloshopify/shopify_bp/views.py
+++ helloshopify/shopify_bp/views.py
@@ -1,5 +1,3 @@
-import pprint
-
 import shopify
 from flask import (
     Blueprint, render_template, current_app, request, redirect, session,
@@ -12,6 +10,7 @@ from ..extensions import db
 
 shopify_bp = Blueprint('shopify_bp', __name__, url_prefix='/shopify')
 
+
 @shopify_bp.route('/')
 @shopify_auth_required
 def index():
@@ -20,6 +19,7 @@ def index():
     """
 
     return render_template('shopify_bp/index.html')
+
 
 @shopify_bp.route('/install')
 def install():
@@ -30,14 +30,16 @@ def install():
     shop_url = request.args.get("shop")
 
     shopify.Session.setup(
-        api_key=current_app.config['SHOPIFY_API_KEY'], 
+        api_key=current_app.config['SHOPIFY_API_KEY'],
         secret=current_app.config['SHOPIFY_SHARED_SECRET'])
 
     session = shopify.Session(shop_url, version='2019-04')
 
-    scope=[
-        "write_products", "read_products", "read_script_tags", 
-        "write_script_tags"]
+    scope = [
+        "write_products", "read_products",
+        "read_script_tags", "write_script_tags",
+        "read_content", "write_content"
+    ]
     redirect_uri = url_for("shopify_bp.finalize", _external=True)
     print(redirect_uri)
     permission_url = session.create_permission_url(
@@ -46,15 +48,16 @@ def install():
     return render_template(
         'shopify_bp/install.html', permission_url=permission_url)
 
+
 @shopify_bp.route('/finalize')
 def finalize():
     """ Generate shop token and store the shop information.
     
     """
-    
+
     shop_url = request.args.get("shop")
     shopify.Session.setup(
-        api_key=current_app.config['SHOPIFY_API_KEY'], 
+        api_key=current_app.config['SHOPIFY_API_KEY'],
         secret=current_app.config['SHOPIFY_SHARED_SECRET'])
     shopify_session = shopify.Session(shop_url, version='2019-04')
 
@@ -67,6 +70,5 @@ def finalize():
     session['shopify_url'] = shop_url
     session['shopify_token'] = token
     session['shopify_id'] = shop.id
-
 
     return redirect(url_for('shopify_bp.index'))
